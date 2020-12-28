9d227402152e36d133f7c489b0e56bed131f5344
AnastassiiaKucher <anastasielisson@gmail.com>
2020-12-28 21:49:22 +0200

    grid

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
-	$('.header-navbar__btn').click(function(){
-		$(this).toggleClass('header-navbar__btn--active');
+	$('.header-navbar__btn').on('click',function(){
+    $(this).toggleClass('header-navbar__btn--active');
+    $('.header-navbar__list').toggClass
+    ('header-navbar__btn--active');
 	});
 });
