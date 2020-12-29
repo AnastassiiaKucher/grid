bf268b7feca66c0b88cd879ef25b2d983f392432
AnastassiiaKucher <anastasielisson@gmail.com>
2020-12-29 07:49:05 +0200

    grid

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,16 @@
 $(document).ready(function(){
 	$('.header-navbar__btn').on('click',function(){
     $(this).toggleClass('header-navbar__btn--active');
-    $('.header-navbar__list').toggClass
+    $('.header-navbar__list').stop(true, true).slideToggle(500);
     ('header-navbar__btn--active');
 	});
 });
+
+
+$window.resize(function(){
+  if ($window.width() > 768){
+    $('header-navbar__list').removeAtr('style');
+    $('header-navbar__btn').removeClass('header-navbar__btn--active');
+  }
+
+})
